d88f9ee1ff616fc5adc7ee1a12864ed9fc2e1a5f
Dandy Nagel <94107254+GittyX3b@users.noreply.github.com>
2026-02-10 17:15:34 +0100

    Initial response displayed, without layers and error handling (#59)
    * restructuring before data displayed
    
    * response displayed, without layers and error handling

--- frontend/src/components/LeafletMap.tsx
+++ frontend/src/components/LeafletMap.tsx
@@ -4,6 +4,8 @@ import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
+import { useSession } from '@data';
+
 const DefaultIcon = L.icon({
   iconUrl: icon,
   shadowUrl: iconShadow,
@@ -13,12 +15,14 @@ const DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 export const LeafletMap = ({ className = '' }: { className?: string }) => {
+  const { locationform } = useSession();
+
   return (
     /* Container MUST have a height */
     <div className={`z-10 min-h-fit w-full bg-white lg:px-20 lg:pt-10 ${className}`}>
       <MapContainer
-        center={[44.19505, 1.95455]}
-        zoom={13}
+        center={[locationform.inputs.latitude!, locationform.inputs.longitude!]}
+        zoom={17}
         scrollWheelZoom={false}
         className='h-full w-full'
       >
@@ -26,15 +30,15 @@ export const LeafletMap = ({ className = '' }: { className?: string }) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={[44.19505, 1.95455]}>
+        <Marker position={[locationform.inputs.latitude!, locationform.inputs.longitude!]}>
           <Popup>MapTold: Your location data starts here.</Popup>
         </Marker>
       </MapContainer>
       <div id='Map-Filter' className='grid h-fit grid-cols-4 gap-3 p-5 lg:px-0'>
-        <button className='btn-mapfilter'>Grünfläche</button>
         <button className='btn-mapfilter'>Gebäude</button>
         <button className='btn-mapfilter'>Strassen</button>
-        <button className='btn-mapfilter'>Toiletten</button>
+        <button className='btn-mapfilter'>Grünfläche</button>
+        <button className='btn-mapfilter'>Gewässer</button>
       </div>
     </div>
   );
